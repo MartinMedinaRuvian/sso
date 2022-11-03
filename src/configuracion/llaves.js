93987083e0a057e471f0a116485932dbc4a7b237
Martin de Jesus Medina Ruvian <44683883+MartinMedinaRuvian@users.noreply.github.com>
2022-11-02 21:32:22 -0500

    Add files via upload

---
+++ src/configuracion/llaves.js
@@ -0,0 +1,14 @@
+module.exports ={
+    configuracionHerokuProduccion:{
+        host:'us-cdbr-east-06.cleardb.net',
+        user:'b22987ef0d328b',
+        password:'a472f6b3',
+        database:'heroku_15a155694911967'
+    },
+    configuracionLocal:{
+        host:'127.0.0.1',
+        user:'root',
+        password:'123abc',
+        database:'single_sign_on'
+    }
+}
