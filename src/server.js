c57753d74e5588456fe65f30cbb35a565fa2ae71
Martin de Jesus Medina Ruvian <44683883+MartinMedinaRuvian@users.noreply.github.com>
2022-11-02 22:08:58 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-require('dotenv').config({ path: './credencialesdb.env' })
+require('dotenv').config({ path: './credenciales.env' })
 
 const app = express();
 
